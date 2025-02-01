52d95abd998d780b361af8beb36a2832ebb59b35
rsaniceto14 <raphaelaniceto14@gmail.com>
2025-02-01 18:52:51 -0300

    [feat] create splash screen with animatedEffects

--- NearbyApp NLW/Sources/Features/Coordinator/NearbyflowController.swift
+++ NearbyApp NLW/Sources/Features/Coordinator/NearbyflowController.swift
@@ -8,4 +8,19 @@
 import Foundation
 import UIKit
 
-
+class NearbyFlowController {
+    private var navigationController: UINavigationController?
+    
+    public init(){
+        
+    }
+    
+    func start() -> UINavigationController? {
+        let contentView = SplashView()
+        let startViewController = SplashViewController(contentView: contentView)
+        self.navigationController = UINavigationController(rootViewController: startViewController)
+        
+        return navigationController
+    }
+    
+}
--- NearbyApp NLW/Sources/Features/Scenes/Splash/SplashView.swift
+++ NearbyApp NLW/Sources/Features/Scenes/Splash/SplashView.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+import UIKit
+
+class SplashView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+//        animatedEffect()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let logoImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "logoImage")
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        return image
+    }()
+    
+    let backgrounImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "backgroundImage")
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+            
+        return image
+    }()
+    
+    private func setupUI() {
+        self.addSubview(logoImageView)
+        self.addSubview(backgrounImage)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            logoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            
+            backgrounImage.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            
+        ])
+        
+    }
+    
+    private func animatedEffect(){
+//        fadeOutEffect() // func sample to using effects on splash
+        //jumpEffect() // can create any effects
+    }
+    
+//    private func fadeOutEffect(){
+//        UIView.animate(withDuration: 1.5, delay: 2.0, options: .curveEaseOut, animations: { self.logoImageView.alpha = 0})
+//        { _ in self.transitionToLogin()}
+//    }
+}
